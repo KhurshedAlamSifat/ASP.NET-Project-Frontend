e78e20dad7d79de8c4fee485780643195520a7b0
Nadim GitHub <nadimhasan753990@gmail.com>
2023-05-16 10:27:33 +0600

    admin front end added

---
+++ Frontend/Admin/JS/dashborad.js
@@ -0,0 +1,49 @@
+var app = angular.module("my-app",[]);
+    app.factory('interceptor',[function(){
+			var interceptor = {
+				request:function(config){
+					config.headers.Authorization = localStorage.getItem("tkey");
+					return config;
+				},
+				response:function(response){
+					return response;
+				}
+			};
+			return interceptor;
+		}]);
+		
+		app.config(function($httpProvider){
+			$httpProvider.interceptors.push('interceptor');
+		});
+		app.controller('AdminCtrl',function($scope,$http){
+			$http.get("https://localhost:44358/api/admins/"+localStorage.getItem("user")).then(function(resp){
+			debugger;
+				$scope.admin = resp.data;
+			},
+			function(err){
+			debugger;
+			});
+		});
+
+
+		app.controller('AdminEditCtrl',function($scope,$http){
+			$http.Post("https://localhost:44358/api/admins/{username}/update"+localStorage.PostItem("user"))+localStorage.postItem("admins").then(function(resp){
+			debugger;
+				$scope.admin = resp.data;
+			},
+			function(err){
+			debugger;
+			});
+		});
+
+      app.controller('logoutCtrl',function($scope,$http){
+			$scope.load=function(){
+				//alert("OK");
+				$http.post("https://localhost:44358/api/logout").then(function(resp){
+					window.location.href = "../../login.html"
+				},function(err){
+					$scope.msg = err.data.Msg;
+				});
+			};
+			
+		});
